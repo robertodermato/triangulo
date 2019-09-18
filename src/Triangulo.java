0ae2d468b0cfb9d563156f42d7f41a7373c31fed
19180346 <mariane1>
2019-09-18 18:57:48 -0300

    sfdsfda

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -17,7 +17,8 @@ public class Triangulo {
 
     public Tipo getTipoAngulo(Triangulo t){
         if (isTriangulo(t.getLadoa(), t.getLadob(), t.getLadoc())==false) return null;
-         -Math.cos(alfa) = (t.getLadoa()*t.getLadoa() - t.getLadob()*t.getLadob() - t.getLadoc()*t.getLadoc())/2*t.getLadob()*t.getLadoc();
+         double x = -(t.getLadoa()*t.getLadoa() - t.getLadob()*t.getLadob() - t.getLadoc()*t.getLadoc())/2*t.getLadob()*t.getLadoc();
+        Math.cos(alfa) = x;
     }
 
     public Tipo getTipoLado(Triangulo t){
