dbddd72e76c57c15f5cfc08f750d7ac9f00399b6
19180346 <mariane1>
2019-09-18 18:41:52 -0300

    tt

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,8 +1,7 @@
 public class Triangulo {
 
     private int ladoa, ladob, ladoc;
-    public enum tipoTriangulo {EQUILATERO, ISOSCELES, ESCALENO, RETANGULO, ACUTANGULO, OBTUSANGULO};
-    private String tipoTriangulo;
+    public enum Tipo {EQUILATERO, ISOSCELES, ESCALENO, RETANGULO, ACUTANGULO, OBTUSANGULO};
 
     public Triangulo (int ladoa, int ladob, int ladoc){
         this.ladoa = ladoa;
@@ -10,12 +9,39 @@ public class Triangulo {
         this.ladoc = ladoc;
     }
 
-    public int getTipoAngulo(Triangulo t){}
+    public int getLadoa() {        return ladoa;    }
 
-    public int getTipoLado(Triangulo t){}
+    public int getLadob() {        return ladob;    }
 
-    public static boolean isTriangulo(int ladoa, int ladob, int ladoc){}
+    public int getLadoc() {        return ladoc;    }
 
-    public String toString(){}
+    public Tipo getTipoAngulo(Triangulo t){
+        if (isTriangulo(t.getLadoa(), t.getLadob(), t.getLadoc())==false) return null;
+
+    }
+
+    public Tipo getTipoLado(Triangulo t){
+
+                if (isTriangulo(t.getLadoa(), t.getLadob(), t.getLadoc())==false) return null;
+                if (t.getLadoa()==t.getLadob() && t.getLadoc()==t.getLadob()) return Tipo.EQUILATERO;
+                if (t.getLadoa()!=t.getLadob() && t.getLadob()!=t.getLadoc() && t.getLadoa()!=t.getLadoc()) return Tipo.ESCALENO;
+                return Tipo.ISOSCELES;
+    }
+
+    public static boolean isTriangulo(int lado1, int lado2, int lado3){
+        if (lado1 > (lado2 + lado3)) {
+            return false;
+        } else if (lado2 > (lado1 + lado3)) {
+            return false;
+        } else if (lado3 > (lado1 + lado2)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public String toString(){
+        return "Triângulo";
+    }
 
 }
