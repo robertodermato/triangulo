b94ddc69f818181ef0cb16ebcafd35ae49999e8e
Roberto <robertodermato@gmail.com>
2019-09-21 14:57:07 -0300

    metodo istriangulo arrumado

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -4,15 +4,10 @@ public class Triangulo {
 
     public enum Tipo {EQUILATERO, ISOSCELES, ESCALENO, RETANGULO, ACUTANGULO, OBTUSANGULO}
 
-    ;
-
     public Triangulo(double ladoa, double ladob, double ladoc) {
-        if (ladoa <= 0) ladoa = 3;
-        else this.ladoa = ladoa;
-        if (ladob <= 0) ladob = 4;
-        else this.ladob = ladob;
-        if (ladoc <= 0) ladoc = 5;
-        else this.ladoc = ladoc;
+        this.ladoa = ladoa;
+        this.ladob = ladob;
+        this.ladoc = ladoc;
     }
 
     public double getLadoa() {
@@ -29,7 +24,7 @@ public class Triangulo {
 
     public Tipo getTipoAngulo() {
 
-        if (isTriangulo(getLadoa(), getLadob(), getLadoc()) == false) return null;
+        if (isTriangulo() == false) return null;
 
         double a2 = getLadoa() * getLadoa();
         double b2 = getLadob() * getLadob();
@@ -45,25 +40,25 @@ public class Triangulo {
     }
 
     public Tipo getTipoLado() {
-        if (isTriangulo(getLadoa(), getLadob(), getLadoc()) == false) return null;
+        if (isTriangulo() == false) return null;
         if (getLadoa() == getLadob() && getLadoc() == getLadob()) return Tipo.EQUILATERO;
-        if (getLadoa() != getLadob() && getLadob() != getLadoc() && getLadoa() != getLadoc())
-            return Tipo.ESCALENO;
+        if (getLadoa() != getLadob() && getLadob() != getLadoc() && getLadoa() != getLadoc()) return Tipo.ESCALENO;
         return Tipo.ISOSCELES;
     }
 
-    public static boolean isTriangulo(double lado1, double lado2, double lado3) {
-        if (lado1 > (lado2 + lado3)) {
+    public boolean isTriangulo() {
+        if (ladoa <= 0 || ladob <= 0 || ladoc <= 0) return false;
+        if (ladoa > (ladob + ladoc)) {
             return false;
-        } else if (lado2 > (lado1 + lado3)) {
+        } else if (ladob > (ladoa + ladoc)) {
             return false;
-        } else if (lado3 > (lado1 + lado2)) {
+        } else if (ladoc > (ladoa + ladob)) {
             return false;
         } else return true;
     }
 
     public String toString() {
-        if (isTriangulo(ladoa, ladob, ladoc))
+        if (isTriangulo())
             return " O triângulo de lados " + ladoa + ", " + ladob + ", " + ladoc + ", " + "é dos tipos: " + getTipoLado() + " e " + getTipoAngulo() + ".";
         else return "Os lados informados não formam um triângulo.";
     }
