2a5862382fbc71e06c7a53858abdd5510a75545a
Roberto <robertodermato@gmail.com>
2019-09-21 18:45:42 -0300

    classe driver terminada

--- src/Driver.java
+++ src/Driver.java
@@ -1,27 +1,44 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class Driver {
 
      @Test //
-     public void testeRetangulo() {
-         System.out.println("Testando retângulo");
+     public void testeTipoAngulo() {
+         //Testando retângulo
          Triangulo tr = new Triangulo(3, 4, 5);
-         if (tr.getTipoAngulo() == Triangulo.Tipo.RETANGULO) System.out.println("Passou no teste");
-         else System.out.println("Teste falhou");
          assertTrue(tr.getTipoAngulo() == Triangulo.Tipo.RETANGULO);
+
+         //Testando obtusângulo
+         Triangulo to = new Triangulo(3, 3, 5);
+         assertTrue(to.getTipoAngulo() == Triangulo.Tipo.OBTUSANGULO);
+
+         //Testando acutângulo
+         Triangulo ta = new Triangulo(3, 3, 3);
+         assertTrue(ta.getTipoAngulo() == Triangulo.Tipo.ACUTANGULO);
      }
 
     @Test //
-    public void testeObtusangulo() {
-        System.out.println("Testando obtusângulo");
-        Triangulo tr = new Triangulo(3, 3, 5);
-        if (tr.getTipoAngulo() == Triangulo.Tipo.OBTUSANGULO) System.out.println("Passou no teste");
-        else System.out.println("Teste falhou");
-        assertTrue(tr.getTipoAngulo() == Triangulo.Tipo.OBTUSANGULO);
+    public void testeTipoLado() {
+        //Testando escaleno
+        Triangulo te = new Triangulo(3, 4, 5);
+        assertTrue(te.getTipoLado() == Triangulo.Tipo.ESCALENO);
+
+        //Testando isósceles
+        Triangulo ti = new Triangulo(3, 3, 5);
+        assertTrue(ti.getTipoLado() == Triangulo.Tipo.ISOSCELES);
+
+        //Testando equilátero
+        Triangulo teq = new Triangulo(3, 3, 3);
+        assertTrue(teq.getTipoLado() == Triangulo.Tipo.EQUILATERO);
+    }
+
+    @Test
+    public void testeIsTriangulo(){
+        //Testando se é triangulo e nesse caso não é
+        Triangulo t = new Triangulo(3, 3, 7);
+        assertTrue(t.isTriangulo() == false );
     }
 
 }
